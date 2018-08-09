df16ffba74435d9ac55d15271deddc10178f35aa
Amstrom <sahandilshan222@gmail.com>
2018-08-09 20:56:26 +0530

    Divisible_Sum_Pairs

---
+++ Divisible_Sum_Pairs.java
@@ -0,0 +1,17 @@
+public class Divisible_Sum_Pairs {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int count=0;
+        for (int i=0;i<n;i++)
+        {
+            for (int j=i+1;j<n;j++)
+            {
+                int x=ar[i]+ar[j];
+                if(x%k==0)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+}
