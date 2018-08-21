4e2e7b2acecfa492e86419a8885e13a33a3b386b
Amstrom <sahandilshan222@gmail.com>
2018-08-21 14:49:10 +0530

    Counting Valleys

--- Sock_Merchant.java
+++ Sock_Merchant.java
@@ -4,7 +4,7 @@ public class Sock_Merchant {
     static int sockMerchant(int n, int[] ar) {
         int count=0;
         Arrays.sort(ar);
-        for(int i=0;i<ar.length;i++) {
+        for(int i=0;i<ar.length;i++) {// to avoid arrays out of bounds exception
             if (i != ar.length - 1) {
                 if (ar[i] == ar[i + 1]) {
                     i++;
