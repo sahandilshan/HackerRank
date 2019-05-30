a403af2219edd29fdb793c9dc3b96ad6357853ab
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2019-05-30 11:37:02 +0530

    Angry Professor Problem

---
+++ Angry_Professor.java
@@ -0,0 +1,16 @@
+public class Angry_Professor {
+    static String angryProfessor(int k, int[] a) {
+
+        int count = 0;
+        for (int x:a)
+        {
+            if(x<=0)
+                count++;
+        }
+
+        if(count>=k)
+            return "NO";
+        return "YES";
+
+    }
+}
