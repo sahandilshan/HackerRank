0b096285190e8a8c03286d276e2216f04f8c3229
Sahan Dilshan <32576163+Amstrom@users.noreply.github.com>
2019-05-30 12:30:59 +0530

    Save The Prisoner and Viral Advertising Problem

---
+++ Viral_Advertising.java
@@ -0,0 +1,17 @@
+public class Viral_Advertising {
+    static int viralAdvertising(int n) {
+        int shared = 2;
+        int total = 2;
+
+        for(int i=1;i<n;i++)
+        {
+            shared = (shared*3)/2;
+            total+=shared;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(viralAdvertising(5));
+    }
+}
